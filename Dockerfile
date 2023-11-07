4e4e81ac7e00b9bc9ed237672188407550f4e36b
billmills <you@example.com>
2023-11-07 17:17:59 -0500

    setting up for production db load

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,9 @@ FROM python:3.9
 
 RUN apt-get update -y ; apt-get install -y nano netcdf-bin
 RUN pip install xarray netcdf4 numpy scipy pymongo shapely geopy argovisHelpers
+WORKDIR /app
+COPY populate-AR.py populate-AR.py
+COPY loaddata.sh loaddata.sh
+COPY parameters/basinmask_01.nc parameters/basinmask_01.nc
+RUN chown -R 1000660000 /app
+
---
+++ loaddata.sh
@@ -0,0 +1,23 @@
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2000.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2001.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2002.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2003.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2004.nc
+python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2005.nc
+python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2006.nc
+python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2007.nc
+python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2008.nc
+python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2009.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2010.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2011.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2012.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2013.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2014.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2015.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2016.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2017.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2018.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2019.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2020.nc
+# python populate-AR.py /tmp/Rutz_ARCatalog_MERRA2_2021.nc
+
